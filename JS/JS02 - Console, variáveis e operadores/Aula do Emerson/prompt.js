c32b0d1d4c9cf0125c92a278d3115ab233366235
EugenioBN <eugenioborellineto@gmail.com>
2024-10-16 21:39:30 -0300

    new file:   "JS/JS02 - Console, vari\303\241veis e operadores/Aula do Emerson/index.html" new file:   "JS/JS02 - Console, vari\303\241veis e operadores/Aula do Emerson/prompt.js"

---
+++ JS/JS02 - Console, variáveis e operadores/Aula do Emerson/prompt.js
@@ -0,0 +1,9 @@
+window.alert("Ola mundo")
+
+let nome = prompt("Digite seu nome")
+window.alert(`${nome} boas vindas`)
+
+let idade = prompt ("Digite sua idade")
+
+idade = idade >= 18 ? "Você é de maior" : "Você não é de maior"
+window.alert(`${idade}`)
