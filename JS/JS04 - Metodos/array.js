58343716d3a43b927c0d2353b03f5b3f0cfe0c56
EugenioBN <eugenioborellineto@gmail.com>
2024-10-17 21:16:10 -0300

    renamed:    "JS/JS02 - Console, vari\303\241veis e operadores/Aula do Emerson/index.html" -> JS/JS03 - Operadores/index.html 	renamed:    "JS/JS02 - Console, vari\303\241veis e operadores/Aula do Emerson/prompt.js" -> JS/JS03 - Operadores/prompt.js 	new file:   JS/JS04 - Metodos/Exercicio/index.html 	new file:   JS/JS04 - Metodos/array.js 	new file:   JS/JS04 - Metodos/index.html

---
+++ JS/JS04 - Metodos/array.js
@@ -0,0 +1,27 @@
+let grupo01 = [false, "Gabi", "Jeferson", 8]
+//        0      1          2             3      
+
+console.log(grupo01[2]);
+console.log(typeof grupo01[2]);
+
+console.log(grupo01[0]);
+console.log(typeof grupo01[0]);
+
+console.log(grupo01[3]);
+console.log(typeof grupo01[3]);
+
+
+
+
+let carro = ["Chevette", "Saveiro", "Fiorino", "Corsa", "Logan"]
+
+console.log(carro[4]);
+console.log(carro[3]);
+
+
+let cupons = "Desc10" // 10%
+// . nome ()
+let menor = cupons.toLowerCase()
+console.log(menor);
+let maior = cupons.toUpperCase()
+console.log(maior)
