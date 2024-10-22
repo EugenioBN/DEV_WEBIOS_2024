0d132673ac14fb4d21e1c6b9bad8913dd32f4dd6
EugenioBN <eugenioborellineto@gmail.com>
2024-10-21 21:41:48 -0300

    new file:   JS/JS04/JS04 - Condicionais/index.html 	new file:   JS/JS04/JS04 - Condicionais/main.js 	renamed:    JS/JS04 - Metodos/Exercicio/index.html -> JS/JS04/JS04 - Metodos/Exercicio/index.html 	renamed:    JS/JS04 - Metodos/array.js -> JS/JS04/JS04 - Metodos/array.js 	renamed:    JS/JS04 - Metodos/index.html -> JS/JS04/JS04 - Metodos/index.html

---
+++ JS/JS04/JS04 - Condicionais/main.js
@@ -0,0 +1,26 @@
+let caioComBlusa = true
+
+if(caioComBlusa == true){
+    console.log("Caio pode assistir a aula!")}
+    else{ // false
+console.log("Caio você não vai assistir a aula e terá uma conversa com a mãe da Gabriella")}
+
+console.clear
+
+let pontos = 500
+
+if(pontos >= 1000){
+    console.log("Passou para a fase 2")
+} // false
+
+console.clear()
+
+// numero
+
+let imparPar = 18
+
+if(imparPar % 2 == 0){ // true
+    console.log("Par")
+}else{
+    console.log("Impar")
+}
