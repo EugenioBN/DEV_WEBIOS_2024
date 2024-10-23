5b24d45ee56d735e67a8e415f324b6d9de1035c9
EugenioBN <eugenioborellineto@gmail.com>
2024-10-22 21:44:01 -0300

    new file:   JS/JS04/JS04 - Emerson/array.js 	new file:   JS/JS04/JS04 - Emerson/index.html 	modified:   JS/JS04/JS04 - Metodos/index.html

---
+++ JS/JS04/JS04 - Emerson/array.js
@@ -0,0 +1,71 @@
+/*let media, faltas;
+media = 10
+faltas = 4
+// 6.5
+if (media >= 7 && faltas <= 5){
+    console.log(`Aluno Aprovado.`);
+} else if (media >= 6.5 && faltas <= 5) {
+    console.log(`Aluno Ap. Conselho.`);
+} else {
+    console.log(`Aluno Reprovado.`);
+}
+console.log(`Fim.`);*/
+
+/*let peso = 90
+let altura = 1.80
+let imc = peso / (altura * altura);
+
+if (imc < 18.5) {
+    console.log("Normal");
+} else if (imc >= 18.5 && imc < 25) {
+    console.log("Magro");
+} else {
+    console.log("Gordo");
+}*/
+
+/*let valor = 2;
+
+switch (valor) {
+    case 1:
+        console.log(`Você foi avaliado com ÓTIMO`)
+        break;
+    case 2:
+        console.log(`Você foi avaliado com BOM`)
+        break;
+    case 3:
+        console.log(`Você foi avaliado com REGULAR`)
+        break;
+    case 4:
+        console.log(`Você foi avaliado com RUIM`)
+        break;
+    case 5:
+        console.log(`Você foi avaliado com PÉSSIMO`)
+        break;
+
+        default:
+            console.log(`Você não foi avaliado`);
+            break;
+}*/
+
+/*let contador = 0;
+
+while (contador <= 1000) {
+console.log(`Valor: ${contador}`);
+contador+=10
+}
+console.log(`FIM`);*/
+
+/*let num = 0
+do {
+    console.log(`Valor: ${num}`);
+    num++
+}   while (num >= 5)
+    console.log(`FIM`);*/
+
+    /*let grupoAlunos = ["Nicolas", "Andrew", "Pasti", "Caio"]
+
+    for (let 1 = 0; 1 < grupoAlunos.length; i++) {
+        console.log(`Olá, ${grupoAlunos[i]}!`);
+    }*/
+
+        
