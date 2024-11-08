6ad227f6540854dafaa92985e57b678ad24d247d
EugenioBN <eugenioborellineto@gmail.com>
2024-11-07 21:30:07 -0300

    JS09

--- JS/JS07 - ArrayObj/Exercicio casa/main.js
+++ JS/JS07 - ArrayObj/Exercicio casa/main.js
@@ -1,20 +1,17 @@
-let alunos = [
-    { nome: 'Ana', idade: 17, nota: 8, ano: '2°B' },
-    { nome: 'Marta', idade: 15, nota: 5, ano: '3°C' },
-    { nome: 'Bruno', idade: 16, nota: 6, ano: '2°C' },
-    { nome: 'Brenno', idade: 19, nota: 6, ano: '3°C' },
-    { nome: 'Veronica', idade: 16, nota: 9, ano: '2°C' },
-    { nome: 'Maria', idade: 14, nota: 4, ano: '1°F' }
-  ];
+let aluno = [
+  { nome: 'Ana', idade: 17, nota: 8, ano: '2°B' },
+  { nome: 'Marta', idade: 15, nota: 5, ano: '3°C' },
+  { nome: 'Bruno', idade: 16, nota: 6, ano: '2°C' },
+  { nome: 'Brenno', idade: 19, nota: 6, ano: '3°C' },
+  { nome: 'Veronica', idade: 16, nota: 9, ano: '2°C' },
+  { nome: 'Maria', idade: 14, nota: 4, ano: '1°F' }
+];
   
-  console.log(alunos);
+console.log(aluno);
   
-  let infoBrenno = alunos.find(aluno => aluno.nome === 'Brenno');
-  console.log(infoBrenno);
-  
-  let notaMaria = alunos.find(aluno => aluno.nome === 'Maria').nota;
-  console.log(notaMaria);
-  
-  let alunosJSON = JSON.stringify(alunos, null, 2);
-  console.log(alunosJSON);
-  
+console.log(aluno[2])
+
+console.log(aluno[5].nota)
+// JSON > OBJ = parse /// OBJ > JSON = Stringfy
+let JsonAluno = JSON.stringify(aluno)
+console.log(JsonAluno)
--- JS/JS09 - MetodoArray/main.js
+++ JS/JS09 - MetodoArray/main.js
@@ -1,15 +1,17 @@
 // Array
 let frutas = ["uva", "laranja", "manga", "melancia", "kiwi", "abacate", "banana", "morango"] // Coloque o nome de 8 frutas
 
-frutas.forEach(Percorrer())
+frutas.forEach((valor, indice) => {
+    console.log(valor); // conteudo da posição
+    console.log(indice); // número da posição
+})
+
+/*frutas.forEach(Percorrer())
 
              // conteúdo, índice
 function Percorrer(indice, valor){
-    console.log(valor); // conteudo da posição
-    console.log(indice); // número da posição
 }
 
-/*
 for(i = 0; i <= frutas.length ; i++){
     console.log(frutas[i]);
     console.log(i); 
