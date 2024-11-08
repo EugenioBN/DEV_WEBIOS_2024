dfa760400f02466f5ab12053672dd1f82e24ea2b
EugenioBN <eugenioborellineto@gmail.com>
2024-11-07 21:47:07 -0300

    modified:   JS/JS09 - MetodoArray/main.js

--- JS/JS09 - MetodoArray/main.js
+++ JS/JS09 - MetodoArray/main.js
@@ -1,6 +1,11 @@
 // Array
 let frutas = ["uva", "laranja", "manga", "melancia", "kiwi", "abacate", "banana", "morango"] // Coloque o nome de 8 frutas
 
+let nomeUsuario = ["Ana", "Pedro", "Caio"]
+nomeUsuario.forEach((valor, indice) => {
+    
+})
+
 frutas.forEach((valor, indice) => {
     console.log(valor); // conteudo da posição
     console.log(indice); // número da posição
