95e904f8f6a7106e4d0224c1447f49ccd0b1fe3d
EugenioBN <eugenioborellineto@gmail.com>
2024-11-14 21:33:34 -0300

    new file:   .vscode/settings.json 	new file:   JS/JS13 - DOM - 3/index.html 	new file:   JS/JS13 - DOM - 3/main.js

---
+++ JS/JS13 - DOM - 3/main.js
@@ -0,0 +1,16 @@
+let botao = document.querySelector('button')
+
+let olhaNumero = true
+if(olhaNumero === true){
+    botao.style.color = "blue"
+    botao.style.backgroundColor = "green"
+    botao.style.borderRadius = "5px"
+    botao.innerHTML = "Cadastrar"
+}
+
+else{
+    botao.style.color = "red"
+}
+
+let paragrafo = document.querySelector('#paragrafo')
+paragrafo.style.backgroundImage = 'linear-gradient(blue,red)'
